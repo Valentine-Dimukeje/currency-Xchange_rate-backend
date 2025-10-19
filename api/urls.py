2176e0c409fff21ca316d4b1740ccf2c1d031aa3
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-10-19 12:54:57 +0100

    Fix CORS and API routes

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-path("rates/", views.rates),
-path("convert/", views.convert),
+    path('api/rates/', views.rates),
+    path('api/convert/', views.convert),
 ]
--- currency_backend/settings.py
+++ currency_backend/settings.py
@@ -10,13 +10,13 @@ ALLOWED_HOSTS = ["*", "currency-xchange-backend.onrender.com"]
 
 # Apps
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "rest_framework",
     "api",
 ]
