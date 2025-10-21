f42ee92fac06801c485869e4a552f5fdde926fc4
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-10-21 09:30:47 +0100

    urls.py, staticfiles and views.py

--- currency_backend/urls.py
+++ currency_backend/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from api import views  # import your app views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # ✅ include all API routes
+    path("api/rates/", views.rates, name="rates"),
+    path("api/convert/", views.convert, name="convert"),
 ]
-
 
